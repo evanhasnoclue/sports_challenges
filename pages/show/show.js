49a7f6ca783b0b941eaefd4a8f89ca42f89cf535
qixiaoyun <181655466@qq.com>
2019-02-04 00:21:19 +0800

    show

--- pages/show/show.js
+++ pages/show/show.js
@@ -13,15 +13,36 @@ Page({
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-    const query3 = Bmob.Query('Challenges');
+    const query = Bmob.Query('Challenges');
+    query.include('user_id')
     let page = this;
-    query3.get("6EYOKKKl").then(res => {
+    query.get("ZIrt444A").then(res => {
       page.setData({ clickChallenge: res });
-      console.log(page.data)
     }).catch(err => {
       console.log(err)
     })
+    // const query2 = Bmob.Query("Bookings");
+    // query.statTo("where", '{"challenge_id":{"$inQuery":{"where":{"objectId:"6EY0KKKl"},"className":"Challenges"}}}');
+    // query2.find().then(res => {
+    //   console.log(res)
+    // });
+    const queryf = Bmob.Query('Bookings')
+    queryf.equalTo("challenge_id", "==", "ZIrt444A");
+    queryf.include('user_id', 'Users')
+    // query.include('user_id')
+    queryf.count().then(res => {
+      page.setData({actualNum:res})
+      console.log(`共有${res}条记录`)
+      console.log(page.data)
+    });
+    queryf.find().then(res => {
+      page.setData({participants:res})
+    }).catch(err => {
+      console.log(err)
+    })
+
   },
+    
 
   /**
    * Lifecycle function--Called when page is initially rendered
