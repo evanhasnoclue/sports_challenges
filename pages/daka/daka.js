c5c95f04587fecbb4cca85b3cb696d15d2c70495
qixiaoyun <181655466@qq.com>
2019-02-18 18:14:54 +0800

    daka photo

--- pages/daka/daka.js
+++ pages/daka/daka.js
@@ -12,6 +12,64 @@ Page({
   /**
    * Lifecycle function--Called when page load
    */
+  gotoShow: function () {
+    var _this = this
+    wx.chooseImage({
+      count: 9, // 最多可以选择的图片张数，默认9
+      sizeType: ['original', 'compressed'], // original 原图，compressed 压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // album 从相册选图，camera 使用相机，默认二者都有
+      success: function (res) {
+        // success
+        console.log(res)
+        _this.setData({
+          photo: res.tempFilePaths
+        })
+
+        console.log(_this.data.photo[0])
+        var tempFilePaths = res.tempFilePaths
+        var file;
+        for (let item of tempFilePaths) {
+          console.log('itemn', item)
+          file = Bmob.File('abc.jpg', item);
+        }
+        file.save().then(res => {
+          console.log(res.length);
+          console.log(res);
+          _this.setData({
+            photoChoose: res
+          })
+          console.log(_this.data)
+          // const file = res
+          // const query = Bmob.Query('Challenges');
+          // query.set('id', '91f596b27f')
+          // query.set('photo', res[0].url)
+          // query.save().then(res => {
+          //   console.log(res)
+          // })
+        })
+
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+
+  },
+  bindUpload:function(){
+    const query = Bmob.Query('Challenges');
+    query.get(this.data.challenge.objectId).then(res => {
+      console.log(res)
+      res.set('daka_photo', this.data.photoChoose[0].url )
+      res.save()
+    }).catch(err => {
+      console.log(err)
+    })
+  },
+
+
   onLoad: function (options) {
     let page = this;
     let challenge_id = 'ZIrt444A';
